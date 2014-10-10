59cd2f9a8b990cb31c137dcaf1e1e4650570b398
Yasvah <yquemener.iris09@gmail.com>
2014-10-10 09:52:30 +0200

    Revert "Migration du Server W08R2-UPDATE --> W08R2-EAGLE"
    This reverts commit b8fed3e3d50bb66587b1d6e57fa0548f9716b483.

--- DataWareHouse/DataWareHouse/My Project/Settings.Designer.vb
+++ DataWareHouse/DataWareHouse/My Project/Settings.Designer.vb
@@ -57,7 +57,7 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=W08R2-EAGLE;Initial Catalog=DW;Integrated Security=True")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=W08R2-EAGLE;Initial Catalog=DW;User ID=sa")>  _
         Public ReadOnly Property DWConnectionString() As String
             Get
                 Return CType(Me("DWConnectionString"),String)
